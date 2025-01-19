153487d4fde3a87c1a29b785ffcb4db9eb348474
jam-madrigal <jam.madrigal@gmail.com>
2025-01-19 13:01:34 -0800

    Add comparisons, game is playable

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -11,7 +11,12 @@ def choose_random_vtuber(youtubers):
     """Selects a random vtuber from a list, then pops it off that list so it can't be chosen again"""
     return youtubers.pop(random.randint(0, len(youtubers) - 1))
 
-
+def get_higher_subs(a, b):
+    if a['subs'] > b['subs']:
+        higher = a
+    else:
+        higher = b
+    return higher
 choose_random_vtuber(data)
 # make a variable to hold the players score, starts at 0
 score = 0
@@ -23,17 +28,36 @@ print(logo)
 while guessed_correct:
 # make the A the previous B, make the dictionary entry after the current A the next B
     if a_vtuber != {}:
-        a_vtuber = a_vtuber
+        a_vtuber = higher
         b_vtuber = choose_random_vtuber(data)
     else:
         a_vtuber = choose_random_vtuber(data)
         b_vtuber = choose_random_vtuber(data)
+
+    higher = get_higher_subs(a_vtuber, b_vtuber)
 # show the logo at the top, the VS in between A and B while comparing
 # ask the user to compare a random vtuber to another random vtuber, when a vtuber is stored in a new variable in the loop...
     print(f'Compare A: {a_vtuber["name"]}, {a_vtuber["bio"]} Their agency is {a_vtuber["agency"]}.')
     print(vs)
     print(f'Vtuber B: {b_vtuber["name"]}, {b_vtuber["bio"]} Their agency is {b_vtuber["agency"]}.')
-    guess = input('Who has more subscribers? Type "A" or "B"').lower()
-    guessed_correct = False
+    guess = input('Who has more subscribers? Type "A" or "B"\n').lower()
 # if they get it wrong, tell them they were wrong and their final score and end the game
+    if guess == 'a':
+        guess = a_vtuber
+    elif guess == 'b':
+        guess = b_vtuber
+    else:
+        'Invalid choice. Game over'
+        guessed_correct = False
+
 # keep going if they guess correctly, increase their score by one,
+    if higher == guess:
+        score += 1
+        print(f'You guessed correctly. Your current score is {score}')
+    else:
+        print(f'You guessed incorrectly. Your final score was {score}')
+        guessed_correct = False
+
+print('Game over.')
+
+
