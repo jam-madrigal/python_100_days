e443b8a57d8ed884e0cced046131ae4f8ff469f8
jam-madrigal <jam.madrigal@gmail.com>
2025-01-21 11:12:04 -0800

    Add prettytable

--- day_16_start/main.py
+++ day_16_start/main.py
@@ -1,10 +1,17 @@
 # OOP fundamentals, attributes and construction
+import turtle
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
 hermit = Turtle()
 hermit.shape('turtle')
 hermit.color('black', 'purple')
+turtle.forward(100)
+
 my_screen = Screen()
+my_screen.exitonclick()
 
 print(my_screen.canvheight)
-my_screen.exitonclick()
+
+table = PrettyTable()
+
