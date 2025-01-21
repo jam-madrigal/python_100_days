496a54ecd996b09b99331d563082cb2e9831df1a
jam-madrigal <jam.madrigal@gmail.com>
2025-01-21 10:53:22 -0800

    Add turtle and screen

---
+++ day_16_start/main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+hermit = Turtle()
+hermit.shape('turtle')
+hermit.color('black', 'purple')
+my_screen = Screen()
+
+print(my_screen.canvheight)
+my_screen.exitonclick()
